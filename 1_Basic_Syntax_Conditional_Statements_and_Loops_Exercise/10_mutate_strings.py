7f2622f2d90b2bec07592207a817dd6913baaf2c
Kristiyan Dimitrov <155374034+kristiyan-dev@users.noreply.github.com>
2024-01-09 10:33:21 +0100

    upload new problem

--- 1_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/10_mutate_strings.py
+++ 1_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/10_mutate_strings.py
@@ -1,4 +1,10 @@
 first_string = input()
 second_string = input()
+last_printed_string = first_string
 for character_index in range(len(first_string)):
-    print(character_index)
+    lest_part = second_string[:character_index + 1]
+    right_part = first_string[character_index + 1:]
+    new_string = lest_part + right_part
+    if new_string != last_printed_string:
+        print(new_string)
+        last_printed_string = new_string
