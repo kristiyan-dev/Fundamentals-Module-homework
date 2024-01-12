6935cf5c43a7dd17001a6f445bb9e3bd79f78c76
Kristiyan Dimitrov <155374034+kristiyan-dev@users.noreply.github.com>
2024-01-12 21:39:54 +0100

    upload

--- 1_Basic_Syntax_Conditional_Statements_and_Loops/07_patterns.py
+++ 1_Basic_Syntax_Conditional_Statements_and_Loops/07_patterns.py
@@ -4,7 +4,7 @@ for i in range(1, number + 1):
     for j in range(0, i):
         print('*', end='')
     print()
-for i in range(number -1, 0 , -1):
+for i in range(number - 1, 0, -1):
     for j in range(0, i):
         print('*', end='')
     print()
--- 1_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/09_sorting_hat.py
+++ 1_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/09_sorting_hat.py
@@ -1,4 +1,16 @@
-name = int(input())
-
-for i in range(1, name + 1, 2):
-    print(i)
+while True:
+    name = input()
+    if name == 'Welcome!':
+        print("Welcome to Hogwarts.")
+        break
+    elif name == 'Voldemort':
+        print("You must not speak of that name!")
+        break
+    if len(name) < 5:
+        print(f'{name} goes to Gryffindor.')
+    elif len(name) == 5:
+        print(f"{name} goes to Slytherin.")
+    elif len(name) == 6:
+        print(f"{name} goes to Ravenclaw.")
+    else:
+        print(f"{name} goes to Hufflepuff.")
