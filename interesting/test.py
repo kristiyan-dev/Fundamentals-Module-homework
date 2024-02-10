ad2a49e2c9625acf2f57e5b41b52480bc2373fef
Kristiyan Dimitrov <155374034+kristiyan-dev@users.noreply.github.com>
2024-02-10 11:07:21 +0100

    up

--- interesting/test.py
+++ interesting/test.py
@@ -9,13 +9,26 @@ window.title('test')
 # window.configure(background='white')
 
 def first_print():
-    text = 'Hello Word'
+    text = 'Обичам те толкоша много! А ти обичашли ме?'
     text_output = tk.Label(window, text=text)
-    text_output.grid(row=0, column=1)
+    text_output.grid(row=0, column=2)
+def second_print():
+    text = 'Happy'
+    text_output = tk.Label(window, text=text)
+    text_output.grid(row=1, column=2)
+def third_print():
+    text = 'Sad'
+    text_output = tk.Label(window, text=text)
+    text_output.grid(row=2, column=2)
 
 
-first_button = tk.Button(text='Hello', command=first_print)
+first_button = tk.Button(text='click me', command=first_print)
 first_button.grid(row=0, column=0)
+second_button = tk.Button(text='Да', command=second_print)
+second_button.grid(row=1, column=0)
+third_button = tk.Button(text='Не', command=third_print)
+third_button.grid(row=2, column=0)
 
 if __name__ == '__main__':
     window.mainloop()
+
