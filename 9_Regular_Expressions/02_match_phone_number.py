ecb61c6a30b4047f5310660ba148f552e7763846
Kristiyan Dimitrov <155374034+kristiyan-dev@users.noreply.github.com>
2024-03-30 16:54:26 +0100

    up

--- 9_Regular_Expressions/02_match_phone_number.py
+++ 9_Regular_Expressions/02_match_phone_number.py
@@ -1,5 +1,17 @@
+# import re
+# # phone_numbers = input()
+# # number_pattern = r"\+359(?P<separator>[ |-])[2](?P=separator)\d{3}(?P=separator)\d{4}\b"
+# # valid_numbers = [obj.group() for obj in re.finditer(number_pattern,phone_numbers)]
+# # print(*valid_numbers,sep=", ")
+
 import re
-phone_numbers = input()
-number_pattern = r"\+359(?P<separator>[ |-])[2](?P=separator)\d{3}(?P=separator)\d{4}\b"
-valid_numbers = [obj.group() for obj in re.finditer(number_pattern,phone_numbers)]
-print(*valid_numbers,sep=", ")
+numbers = input()
+valid_phone_number = []
+regex = r'\+359-2-\d{3}-\d{4}\b|\+359 2 \d{3} \d{4}\b'
+matches = re.findall(regex, numbers)
+
+for i in range(len(matches)):
+    if i <len(matches) - 1:
+        print(matches[i], end=", ")
+    else:
+        print(matches[i])
